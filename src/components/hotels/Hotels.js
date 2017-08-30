8475b399891ce2f8b678aa8ab9dca8d74acd0789
Raymond <dude92128@aol.com>
2017-08-29 22:10:22 -0700

    Update Hotels.js
    Finished Hotels.js

--- src/components/hotels/Hotels.js
+++ src/components/hotels/Hotels.js
@@ -17,6 +17,7 @@ export default class Hotels extends Component {
                     <h1 id="raffles-pool" className="text-left">Raffles Pool & Spa</h1>
                 </div>
             </div>
+            
             <div className="Thirteen">            
                 <div className="Thirteen-inner">            
                     <div className="container">
@@ -28,33 +29,32 @@ export default class Hotels extends Component {
                 </div>
             </div>
             
-            
             <div className="Fourteen">
-            <ul className="flex-container">
-                    <a  href="https://www.trivago.com/istanbul-32123/hotel"><li className="flex-item">
+                <ul className="flex-container">
+                    <li className="flex-item">
                         <h2>İstanbul Hotels</h2>
-                    </li></a>
-                    <a  href="https://www.trivago.com/izmir-32103/hotel"><li className="flex-item">
+                    </li>
+                    <li className="flex-item">
                         <h2>İzmir Hotels</h2>
-                    </li></a>
-                    <a  href="https://www.trivago.com/antalya-32134/hotel"><li className="flex-item">
+                    </li>
+                    <li className="flex-item">
                         <h2>Antalya Hotels</h2>
-                    </li></a>
-                    <a  href="https://www.trivago.com/ankara-32205/hotel"><li className="flex-item">
+                    </li>
+                    <li className="flex-item">
                         <h2>Ankara Hotels</h2>
-                    </li></a>
-                    <a  href="https://www.trivago.com/nevsehir-509044/hotel"><li className="flex-item">
+                    </li>
+                    <li className="flex-item">
                         <h2>Cappadocia Hotels</h2>
-                    </li></a>
-                    <a  href="https://www.trivago.com/trabzon-32190/hotel/novotel-trabzon-950093"><li className="flex-item">
+                    </li>
+                    <li className="flex-item">
                         <h2>Trabzon Hotels</h2>
-                    </li></a>
-                    <a  href="https://www.trivago.com/bursa-509192/hotel"><li className="flex-item">
+                    </li>
+                    <li className="flex-item">
                         <h2>Bursa Hotels</h2>
-                    </li></a>
-                    <a  href="https://www.trivago.com/konya-509022/hotel"><li className="flex-item">
+                    </li>
+                    <li className="flex-item">
                         <h2>Konya Hotels</h2>
-                    </li></a>
+                    </li>
                 </ul>           
             </div>
         </div>
