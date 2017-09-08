d492b3e4691a85969db238565e231f68a8422ae8
Raymond <dude92128@aol.com>
2017-09-07 22:56:13 -0700

    Scrollmagic classes added

--- src/components/cuisine/Cuisine.js
+++ src/components/cuisine/Cuisine.js
@@ -8,26 +8,26 @@ export default class Cuisine extends Component {
             <div className="Fifteen" id="Fifteen">            
                 <div className="Fifteen-inner">            
                     <div className="container">
-                        <h1 id="mutfak" className="text-center">Mutfak</h1>
-                        <h2 id="cuisine" className="text-center">Cuisine</h2>
-                        <div className="col-lg-6 col-lg-offset-6">
+                        <h1 id="mutfak" className="text-center scrollmagic-header">Mutfak</h1>
+                        <h2 id="cuisine" className="text-center scrollmagic-header">Cuisine</h2>
+                        <div className="col-lg-6 col-lg-offset-6 text-right">
                             <h1 id="meze" className="text-right">Turkish Meze</h1>
-                            <p id="meze-desc">The poster boy of Turkish cuisine, the döner kebab is a world-famous Turkish kebab usually consisting of lamb cooked on a vertical rotisserie.</p>
+                            <p id="meze-desc" className="scrollmagic-desc">Turkish Meze consists of a wide variety of small plates & can be treated an appitizer. Common meze dishes include sliced melon, strained yogurt with herbs, eggplant salad, and fried calamari.</p>
                         </div>
                     </div>
                 </div>
                 <div className="container">
                     <div className="col-lg-6 text-left">
                         <h1 id="doner">Döner Kebab</h1>
-                        <p id="doner-desc">The poster boy of Turkish cuisine, the döner kebab is a world-famous Turkish kebab usually consisting of lamb cooked on a vertical rotisserie.</p>
+                        <p id="doner-desc" className="scrollmagic-desc">The poster boy of Turkish cuisine, the döner kebab is a world-famous Turkish kebab usually consisting of lamb cooked on a vertical rotisserie.</p>
                     </div>
                 </div>
             </div>
             <div className="Sixteen">
                 <ul className="flex-container">
                     <li className="flex-item">
-                        <h1 id="yemegi">Türk yemeği</h1>
-                        <h2 id="food">Turkish food</h2>
+                        <h1 id="yemegi" className="scrollmagic-header">Türk yemeği</h1>
+                        <h2 id="food" className="scrollmagic-header">Turkish food</h2>
                         <p>They say the history of a country can be seen in its food, & Turkish food is certainly no exception. The Greeks, Persians, Arabs, & other groups have all left their mark to form what is one of the most unique yet familiar cuisines in the world. A meal in Turkey can consist of anything from kebabs with massive swaths of meat to stuffed vegetables (Dolma). Popular beverages are Turkish tea & rakı, a clear alcoholic drink from in the Balkans. Turkish delight & baklava are Turkey's most famous desserts. Dig in!</p>
                     </li>
                     <li className="flex-item">
@@ -46,14 +46,14 @@ export default class Cuisine extends Component {
                     <div className="container"> 
                         <div className="col-lg-6 col-lg-offset-6 text-right"> 
                             <h1 id="delight">Turkish Delight</h1>   
-                            <p id="delight-desc">Turkish delight, or 'lokum' in Turkish, is a variety of desserts made from sugar and gel. You can find a plethora of different flavors including pistachio, walnut & cinnamon.</p>
+                            <p id="delight-desc" className="scrollmagic-desc">Turkish delight, or 'lokum' in Turkish, is a variety of desserts made from sugar and gel. You can find a plethora of different flavors including pistachio, walnut & cinnamon.</p>
                         </div>
                     </div>
                 </div>
                 <div className="container">
                     <div className="col-lg-6 text-left">
                         <h1 id="dolma">Dolma</h1>
-                        <p id="dolma-desc">Dolma is a family of stuffed vegetable dishes that can be found all over the Balkans, Middle East, & Central Asia. Common vegetables to stuff include tomato, pepper, onion, zucchini, eggplant, and garlic.</p>
+                        <p id="dolma-desc" className="scrollmagic-desc">Dolma is a family of stuffed vegetable dishes that can be found all over the Balkans, Middle East, & Central Asia. Common vegetables to stuff include tomato, pepper, onion, zucchini, eggplant, and garlic.</p>
                     </div>
                 </div>
                 <div className="container">   
@@ -65,14 +65,14 @@ export default class Cuisine extends Component {
                     <div className="container"> 
                         <div className="col-lg-6 col-lg-offset-6 text-right"> 
                             <h1 id="simit">Simit</h1>   
-                            <p id="simit-desc">Simit is a circular bread with sesame seeds & is the Turkish equivalent of a bagel. Its texture, size, & sweetness varies by region.</p>
+                            <p id="simit-desc" className="scrollmagic-desc">Simit is a circular bread with sesame seeds & is the Turkish equivalent of a bagel. Its texture, size, & sweetness varies by region.</p>
                         </div>
                     </div>
                 </div>
                 <div className="container">
                     <div className="col-lg-6 text-left">
                         <h1 id="kokorec">Kokoreç</h1>
-                        <p id="kokorec-desc">Kokoreç is a dish consisting of lamb or goat intestines wrapped around either seasoned meat or bread. In Turkey it's served mostly as fast-food.</p>
+                        <p id="kokorec-desc" className="scrollmagic-desc">Kokoreç is a dish consisting of lamb or goat intestines wrapped around either seasoned meat or bread. In Turkey it's served mostly as fast-food.</p>
                     </div>
                 </div>
             </div>
