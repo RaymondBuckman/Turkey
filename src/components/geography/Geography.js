9d25b14cb0ba82c6758db08bc806bafac35d9c25
Raymond <dude92128@aol.com>
2017-09-07 22:57:26 -0700

    Scrollmagic classes added

--- src/components/geography/Geography.js
+++ src/components/geography/Geography.js
@@ -93,8 +93,8 @@ export default class Geography extends Component {
             <div className="Three" id="Three">            
                 <div className="Three-inner">            
                     <div className="container">
-                        <h1 id="cografya" className="text-center">Coğrafya</h1>
-                        <h2 id="geography" className="text-center">Geography</h2> 
+                        <h1 id="cografya" className="text-center scrollmagic-header">Coğrafya</h1>
+                        <h2 id="geography" className="text-center scrollmagic-header">Geography</h2> 
                         <h1 className="text-right">Uzungöl, Trabzon</h1>
                     </div>
                 </div>
@@ -105,19 +105,19 @@ export default class Geography extends Component {
             <div className="Four">
                 <div className="Four-inner-1">
                     <div className="container">
-                            <h1 id="merkezi" className="text-center">Dünyanın merkezi</h1>
-                            <h2 id="center" className="text-center">The Center of the World</h2>
-                            <p id="ranging" className="col-xs-12 text-center">Ranging from the Southeastern Balkans to the mountainous southern Caucasus in the east, Turkey sits at the junction of Europe & Asia. Turkey is both geographically & historically the center of the world. You'll find its fog-covered economic & historical center Istanbul in the Northwest, Antalya & its California-like surroundings in the Southwest, the mountainous Black Sea Region in the NorthEast with scenery resembling Lord of the Rings, &  the Kurdish region bordering Iran to the Southeast. In the center rests the capital Ankara along with the incredible & other-worldly Cappadocia region to the South.</p>  
+                            <h1 id="merkezi" className="text-center scrollmagic-header">Dünyanın merkezi</h1>
+                            <h2 id="center" className="text-center scrollmagic-header">The Center of the World</h2>
+                            <p id="ranging" className="col-xs-12 text-center scrollmagic-desc">Ranging from the Southeastern Balkans to the mountainous southern Caucasus in the east, Turkey sits at the junction of Europe & Asia. Turkey is both geographically & historically the center of the world. You'll find its fog-covered economic & historical center Istanbul in the Northwest, Antalya & its California-like surroundings in the Southwest, the mountainous Black Sea Region in the NorthEast with scenery resembling Lord of the Rings, &  the Kurdish region bordering Iran to the Southeast. In the center rests the capital Ankara along with the incredible & other-worldly Cappadocia region to the South.</p>  
                     </div>        
                 </div>
-                <h1 id="ataturk" className="text-right">Mustafa Kemal Atatürk</h1>
-                <h2 id="founder" className="text-right">Founder of the Republic of Turkey</h2> 
+                <h1 id="ataturk" className="text-right scrollmagic-header">Mustafa Kemal Atatürk</h1>
+                <h2 id="founder" className="text-right scrollmagic-header">Founder of the Republic of Turkey</h2> 
             </div>
             <div className="Five">
                 <ul className="flex-container">
                     <li className="flex-item">
-                        <h1 id="bolgeleri">Türkiye'nın Bölgeleri</h1>
-                        <h2 id="regions">Regions of Turkey</h2>
+                        <h1 id="bolgeleri" className="scrollmagic-header">Türkiye'nın Bölgeleri</h1>
+                        <h2 id="regions" className="scrollmagic-header">Regions of Turkey</h2>
                     </li>
                     <li className="flex-item scrollmagic" 
                     onClick={() => this.setState({photoIndex:0, isOpen: true })}>
