7c3052ccd3a288235bee95b6db1231e6a3ec4ad0
Raymond <dude92128@aol.com>
2017-09-24 23:18:41 -0700

    Minor tweaks

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -30,7 +30,8 @@ export default class Intro extends Component {
                         <h2 id="flag" className="scrollmagic-header">The Turkish flag</h2>
                         <p>No one is 100% sure about the origins of the Turkish flag. Some say the crescent moon refers to the Byzantine goddess Artemis while others say that it's an Islamic symbol. The star is assumed to have been adopted from the Byzantine empire, where Emporer Constantine decided Istanbul (then Constantinople) to the Virgin Mary. The flag was used during the Ottoman Empire & the proportions were slightly altered when it became a republic on October 29, 1923.</p>
                     </li>
-                    <li className="flex-item scrollmagic-info-squares"></li>
+                    <li className="flex-item scrollmagic-info-squares">
+                    </li>
                     <li className="flex-item scrollmagic-info-squares">
                         <h2>Coat of Arms of the Ottoman Empire</h2>
                     </li>
