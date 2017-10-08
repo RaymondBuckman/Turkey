0587d1b6a77fdc020fbbd2af897b8259dc2b4792
Raymond <dude92128@aol.com>
2017-10-07 23:51:36 -0700

    Accessibility roles added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,13 +9,13 @@ export default class Footer extends Component {
                     <h1 id="yolculuk" className="text-left scrollmagic-header">İyi Yolculuk!</h1>
                     <h2 id="voyage" className="text-left scrollmagic-header">Bon voyage!</h2>
                 </div>
-                <nav id="bottom-nav" className="navbar navbar-default visible-lg" data-spy="affix" role="navigation">
+                <nav id="bottom-nav" className="navbar navbar-default visible-lg" data-spy="affix">
                         <div className="container">
                             <ul className="nav navbar-nav navbar-right">
-                                <li><a href="http://www.historyworld.net/wrldhis/PlainTextHistories.asp?historyid=ac94" target="_blank">History</a></li>
-                                <li><a href="http://www.turkishlanguage.co.uk/" target="_blank">Detailed Language Info</a></li>
-                                <li><a href="http://www.turkishculture.org/" target="_blank">Culture</a></li>
-                                <li><a href="https://www.themuse.com/advice/6-tips-for-traveling-to-turkey" target="_blank">Travel Tips</a></li>
+                                <li><a href="http://www.historyworld.net/wrldhis/PlainTextHistories.asp?historyid=ac94" target="_blank" role="link">History</a></li>
+                                <li><a href="http://www.turkishlanguage.co.uk/" target="_blank" role="link">Detailed Language Info</a></li>
+                                <li><a href="http://www.turkishculture.org/" target="_blank" role="link">Culture</a></li>
+                                <li><a href="https://www.themuse.com/advice/6-tips-for-traveling-to-turkey" target="_blank" role="link">Travel Tips</a></li>
                             </ul>
                         </div>
                 </nav>
