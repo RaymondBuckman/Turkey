740b2c14bd1a5c8a67d26a2d281699a7c3c95f1e
Raymond <dude92128@aol.com>
2017-10-14 23:58:13 -0700

    Additional accessibility

--- src/components/hotels/Hotels.js
+++ src/components/hotels/Hotels.js
@@ -9,7 +9,7 @@ export default class Hotels extends Component {
                 <div className="Twelve-inner scrollmagic-inner-fadeout">           
                     <div className="container">
                         <h1 id="oteleler" className="text-center scrollmagic-header" role="heading">Oteleler</h1>
-                        <h2 id="hotels" className="text-center scrollmagic-header" role="heading">Hotels</h2> 
+                        <h2 id="hotels" className="text-center scrollmagic-header" role="heading" tabIndex="0">Hotels</h2> 
                         <h1 id="raffles1" className="text-right scrollmagic-desc">Raffles Hotel, İstanbul</h1>
                         <h2 id="raffles1-desc" className="text-right scrollmagic-desc">Lobby</h2>
                     </div>
@@ -21,7 +21,8 @@ export default class Hotels extends Component {
             </div>
             
             <div className="Thirteen scrollmagic-div-fadeout">           
-                <div className="Thirteen-inner scrollmagic-inner-fadeout">         <div className="container">
+                <div className="Thirteen-inner scrollmagic-inner-fadeout">         
+                    <div className="container">
                         <h1 id="kayakapi1" className="text-right scrollmagic-desc">Kayakapi Hotel, Ürgüp</h1>
                         <h2 id="kayakapi1-desc" className="text-right scrollmagic-desc">Accomadations</h2>
                     </div>
