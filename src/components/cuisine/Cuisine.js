dffa0807b2882a19e13f98f457db3f7addbd4d2e
Raymond <dude92128@aol.com>
2017-10-14 23:56:29 -0700

    Additional accessibility

--- src/components/cuisine/Cuisine.js
+++ src/components/cuisine/Cuisine.js
@@ -9,7 +9,7 @@ export default class Cuisine extends Component {
                 <div className="Fifteen-inner scrollmagic-inner-fadeout">            
                     <div className="container">
                         <h1 id="mutfak" className="text-center scrollmagic-header" role="heading">Mutfak</h1>
-                        <h2 id="cuisine" className="text-center scrollmagic-header" role="heading">Cuisine</h2>
+                        <h2 id="cuisine" className="text-center scrollmagic-header" role="heading" tabIndex="0">Cuisine</h2>
                         <div className="col-xs-10 col-xs-offset-2 col-lg-6 col-lg-offset-6 text-right">
                             <h1 id="meze" className="text-right scrollmagic-desc">Turkish Meze</h1>
                             <p id="meze-desc" className="scrollmagic-desc">Turkish Meze consists of a wide variety of small plates & can be treated like an appitizer. Common meze dishes include sliced melon, strained yogurt with herbs, eggplant salad, and fried calamari.</p>
@@ -27,7 +27,7 @@ export default class Cuisine extends Component {
                 <ul className="flex-container">
                     <li className="flex-item scrollmagic-info-squares">
                         <h1 id="yemegi" className="scrollmagic-header">Türk yemeği</h1>
-                        <h2 id="food" className="scrollmagic-header">Turkish food</h2>
+                        <h2 id="food" className="scrollmagic-header" tabIndex="0">Turkish food</h2>
                         <p>They say the history of a country can be seen in its food, & Turkish food is certainly no exception. The Greeks, Persians, Arabs, & other groups have all left their mark to form what is one of the most unique yet familiar cuisines in the world. A meal in Turkey can consist of anything from kebabs with massive swaths of meat to stuffed vegetables. Popular beverages are Turkish tea & rakı, a clear alcoholic drink from in the Balkans. Turkish delight & baklava are Turkey's most famous desserts. Dig in!</p>
                     </li>
                     <li className="flex-item scrollmagic-info-squares">
