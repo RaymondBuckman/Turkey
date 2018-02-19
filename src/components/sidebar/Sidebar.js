62c8ce02f1fd247989729a2d75bf8b60e5ff3b0f
Raymond <dude92128@aol.com>
2018-02-18 23:44:20 -0800

    Update Sidebar.js

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -10,10 +10,10 @@ export default class Sidebar extends Component {
                 <li><a href="#top-of-page" alt="jump to the top of the page"><img src="https://png.icons8.com/moon-star-filled/ios7/75/FFFFFF" height="25px" data-tip data-for="crescent-star-tooltip"></img></a></li>
                 <li><a href="#cografya" role="link"><img src="https://png.icons8.com/mountain-filled/ios7/75/FFFFFF"  width="25" height="25" data-tip data-for="geography-tooltip"></img></a></li>
                 <li><a href="#sehirler" role="link"><img src="https://png.icons8.com/city-filled/ios7/75/FFFFFF" width="25" height="25" data-tip data-for="cities-tooltip"></img></a></li>
-                <li><a href="#Sights-1" role="link"><img src="https://png.icons8.com/tourist-guide-filled/ios7/75/FFFFFF" width="25" height="25" data-tip data-for="sights-tooltip"></img></a></li>
+                <li><a href="#sights-1-div" role="link"><img src="https://png.icons8.com/tourist-guide-filled/ios7/75/FFFFFF" width="25" height="25" data-tip data-for="sights-tooltip"></img></a></li>
                 <li><a href="#oteleler" role="link"><img src="https://png.icons8.com/bed-filled/ios7/75/FFFFFF" width="25" height="25" data-tip data-for="hotels-tooltip"></img></a></li>
                 <li><a href="#mutfak" role="link"><img src="https://png.icons8.com/food-and-wine-filled/ios7/75/FFFFFF" width="25" height="25" data-tip data-for="cuisine-tooltip"></img></a></li>
-                <li><a href="#Language-1" role="link"><img src="https://png.icons8.com/communication-filled/ios7/75/FFFFFF" width="25" height="25" data-tip data-for="language-tooltip"></img></a></li>
+                <li><a href="#language-1-div" role="link"><img src="https://png.icons8.com/communication-filled/ios7/75/FFFFFF" width="25" height="25" data-tip data-for="language-tooltip"></img></a></li>
             </ul>
             <ReactTooltip id="crescent-star-tooltip" place="right" type="light" effect="solid" delayShow={100} offset={{left: 15}}>
                 <span className="tooltip-span">Top of Page<span className="tooltip-arrow">&nbsp;&nbsp;&#8594;</span></span>
